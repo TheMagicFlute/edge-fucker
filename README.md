a3cb23bf9b57d645232398f74457cc48799d7d8e
TheMagicFlute <The_Magic_Flute@outlook.com>
2025-05-28 04:46:29 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# edge-fucker
+
+help you to kill the fucking edge browser
+
+you should stop it by the taskmgr.exe
+
+## license
+ 
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE (WTFPL)
+
---
+++ edge-fucker.cpp
@@ -0,0 +1,53 @@
+﻿#include <windows.h>
+#include <tlhelp32.h>
+#include <string>
+#include <WinBase.h>
+
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+
+bool process_is_exist(const std::string& process_name)  
+{  
+   HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);  
+   if (hSnapshot == INVALID_HANDLE_VALUE)  
+       return false;  
+   PROCESSENTRY32 pe;  
+   pe.dwSize = sizeof(PROCESSENTRY32);  
+   if (!Process32First(hSnapshot, &pe))  
+   {  
+       CloseHandle(hSnapshot);  
+       return false;  
+   }  
+   do  
+   {  
+       // Convert WCHAR to std::string for comparison  
+       std::wstring wstrExeFile(pe.szExeFile);  
+       std::string strExeFile(wstrExeFile.begin(), wstrExeFile.end());  
+       if (process_name == strExeFile)  
+       {  
+           CloseHandle(hSnapshot);  
+           return true;  
+       }  
+   } while (Process32Next(hSnapshot, &pe));  
+   CloseHandle(hSnapshot);  
+   return false;  
+}
+
+int main()
+{
+    while (true)
+    {
+        // none-stop fuck the edge & ie
+        if (process_is_exist("msedge.exe"))
+        {
+            system("taskkill /f /im msedge.exe");
+        }
+        // ie
+        if (process_is_exist("iexplore.exe"))
+        {
+            system("taskkill /f /im iexplore.exe");
+        }
+        Sleep(1000);
+    }
+
+    return 0;
+}
